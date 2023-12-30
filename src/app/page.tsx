79b2af4465d2dccc33058c2a53598efe3b00b88a
nishchal <singhnishchal27@gmail.com>
2023-12-30 22:41:35 +0530

    content updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
           <p className="text-sm font-semibold text-gray-700">
-            Abhi is now public!
+            Abhi File Manager is now public!
           </p>
         </div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-          Chat with your <span className="text-blue-600">documents</span> in
-          seconds.
+          upload your <span className="text-blue-600">documents</span> in
+          seconds. and Mnanage them in one place.
         </h1>
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
           Abhi allows you to have conversations with any PDF document. Simply
@@ -87,10 +87,10 @@ export default function Home() {
         <div className="mb-12 px-6 lg:px-8">
           <div className="mx-auto max-w-2xl sm:text-center">
             <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
-              Start chatting in minutes
+              Start uploading in minutes
             </h2>
             <p className="mt-4 text-lg text-gray-600">
-              Chatting to your PDF files has never been easier than with abhi.
+              Uploading your PDF files has never been easier than with abhi File Manager.
             </p>
           </div>
         </div>
